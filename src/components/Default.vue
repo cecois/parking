7b4c8d03ebd0c3550f7f711adbb5229672166fc9
cecois <cecmcgee@gmail.com>
2018-12-01 12:27:05 -0500

    z-indices all fixed up

--- src/components/Default.vue
+++ src/components/Default.vue
@@ -2,16 +2,16 @@
 <div>
 
 <div id="notmap" class="columns">
-  <div id="brand" class="column is-one-half">
-    <!-- I'm a Lot -->
+  <div id="brand" class="column is-one-quarter">
+    I'm a Lot
   </div>
-  <div id="" class="column is-one-half has-text-grey-light is-size-7 has-text-right is-lowercase">
+  <div id="" class="column is-three-quarters has-text-grey-light is-size-7 has-text-right is-lowercase">
     <div style="width:100%;height:80%;">
       <div id="console" v-bind:class="{'is-invisible':(this.temp.msg=='')}">{{this.temp.msg}}</div>
     </div>
     <div style="width:100%;height:20%;">
       <small id="attribution">
-      &copy; data:<a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; basemap ({{this.basemap.nom}}): {{this.basemap.source}}
+      &copy; code:C.C.Miller, &copy; data:<a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; basemap ({{this.basemap.nom}}): {{this.basemap.source}}
     </small>
     </div>
 
@@ -48,39 +48,32 @@ export default {
   data () {
     return {
       basemaps:[
-      {source:"carto.com",key:"dark_all",nom:"Carto Dark",uri:"http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}@2x.png",active:true}
-      ,{source:"carto.com",key:"light_all",nom:"Carto Light",uri:"http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}@2x.png",active:false}
-      ,{source:"esri",key:"esri_gray_dark",nom:"Esri Gray Dark",uri:"http://services.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Dark_Gray_Base/MapServer/tile/{z}/{y}/{x}",active:false}
-      ,{source:"esri",key:"esri_gray_light",nom:"Esri Gray Light",uri:"http://services.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}",active:false}
-      ,{source:"esri",key:"esri_natgeo",nom:"Esri National Geographic",uri:"http://services.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}",active:false}
-      ,{source:"wmflabs.org",key:"mapnik_bw",nom:"Mapnik B/W",uri:"http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png",active:false}
-      ,{source:"stamen.com",key:"stamen_toner",nom:"Stamen Toner",uri:"http://tile.stamen.com/toner/{z}/{x}/{y}.png",active:false}
-      ,{source:"stamen.com",key:"stamen_toner_lite",nom:"Stamen Toner Light",uri:"http://tile.stamen.com/toner-lite/{z}/{x}/{y}.png",active:false}
-      ,{source:"stamen.com",key:"stamen_watercolor",nom:"Stamen Watercolor",uri:"http://tile.stamen.com/watercolor/{z}/{x}/{y}.jpg",active:false}
-      ,{source:"waze.com",key:"waze_us",nom:"Waze US",uri:"https://livemap-tiles3.waze.com/tiles/{z}/{x}/{y}.png",active:false}
-      ,{source:"yandex.net",key:"yandex",nom:"Yandex",uri:"http://vec01.maps.yandex.net/tiles?l=map&x={x}&y={y}&z={z}",active:false}
-      ,{active:false,nom:"AJ Ashton's Pencil",source:"mapbox.com",key:"mapbox_ashton_pencil",uri:"http://{s}.tiles.mapbox.com/v4/aj.03e9e12d/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejdueG92YjAwZHUzMnA5ZWIyMW1zcDQifQ.acDRLVcqW0LZfWQXvC3-pw"}
-,{active:false,nom:"Cloudmade Grey",source:"cloudmade.com",key:"cloudmade_grey",uri:"http://{s}.tile.cloudmade.com/4f5c5233516d4c39a218425764d98def/22677/256/{z}/{x}/{y}.png"}
-,{active:false,nom:"Cloudmade Red Alert",source:"cloudmade.com",key:"cloudmade_redalert",uri:"http://{s}.tile.cloudmade.com/4f5c5233516d4c39a218425764d98def/19996/256/{z}/{x}/{y}.png"}
-,{active:false,nom:"Pinterest by Stamen",source:"stamen.com",key:"stamen_pinterest",uri:"http://{s}.tiles.mapbox.com/v3/pinterest.map-ho21rkos/{z}/{x}/{y}.png"}
-,{active:false,nom:"Woodcut by Eleanor Lutz",source:"mapbox.com",key:"mapbox_lutz_woodcut",uri:"http://{s}.tiles.mapbox.com/v4/mapbox.b0v97egc/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejdueG92YjAwZHUzMnA5ZWIyMW1zcDQifQ.acDRLVcqW0LZfWQXvC3-pw"}
-,{active:false,nom:"Space Station by Eleneor Lutz",source:"mapbox.com",key:"mapbox_lutz_spacestation",uri:"http://{s}.tiles.mapbox.com/v3/eleanor.ipncow29/{z}/{x}/{y}.png"}
-,{active:false,nom:"MapBox Financial Times",source:"mapbox.com",key:"mapbox_financialtimes",uri:"http://{s}.tiles.mapbox.com/v3/financialtimes.map-w7l4lfi8/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZHVuY2FuZ3JhaGFtIiwiYSI6IlJJcWdFczQifQ.9HUpTV1es8IjaGAf_s64VQ"}
-,{active:false,nom:"MapBox InfoAmazonia (Color)",source:"mapbox.com",key:"mapbox_infoamazonia_color",uri:"http://{s}.tiles.mapbox.com/v3/infoamazonia.map-ihpa46c3/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZHVuY2FuZ3JhaGFtIiwiYSI6IlJJcWdFczQifQ.9HUpTV1es8IjaGAf_s64VQ"}
-,{active:false,nom:"Mapnik Mono",source:"wmflabs.org",key:"mapnik_mono",uri:"http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png"}
-,{active:false,nom:"Blueprint by Amy Lee Walton",source:"mapbox.com",key:"mapbox_mslee_blueprint",uri:"http://api.mapbox.com/styles/v1/mslee/ciellcr9y001g5pknxuqwjhqm/tiles/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoibXNsZWUiLCJhIjoiclpiTWV5SSJ9.P_h8r37vD8jpIH1A6i1VRg"}
-,{active:false,nom:"Winter by Amy Lee Walton",source:"mapbox.com",key:"mapbox_mslee_winter",uri:"http://api.mapbox.com/styles/v1/mslee/cij1zoclj002y8rkkdjl69psd/tiles/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoibXNsZWUiLCJhIjoiclpiTWV5SSJ9.P_h8r37vD8jpIH1A6i1VRg"}
-,{active:false,nom:"Standard Oil by Saman Bemel Benrud",source:"mapbox.com",key:"mapbox_saman_standardoil",uri:"http://api.mapbox.com/styles/v1/saman/cif61g3y602sju1m5b02255c9/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic2FtYW4iLCJhIjoiS1ptdnd0VSJ9.19qza-F_vXkgpnh80oZJww"}
-,{active:false,nom:"Carto World Antique",source:"carto.com",key:"carto_antique",uri:"http://cartocdn_d.global.ssl.fastly.net/base-antique/{z}/{x}/{y}.png"}
-,{active:false,nom:"Camouflage by Eden Halperin",source:"mapbox.com",key:"mapbox_edenhalperin_camouflage",uri:"http://api.mapbox.com/styles/v1/edenhalperin/cifq0r0e5000q85m0d293k6mq/tiles/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiZWRlbmhhbHBlcmluIiwiYSI6IlFRZG0zMWMifQ.QUNKx4tIMjZfwmrE8SE6Bg"}
-,{active:false,nom:"MapBox Comic!",source:"mapbox.com",key:"mapbox_comic",uri:"https://{s}.tiles.mapbox.com/v4/mapbox.comic/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiYmVydHQiLCJhIjoiaWFvN1Z4USJ9.k04Q6CVms4Gx7KDZxvAx2A"}
-,{active:false,nom:"MapBox Emerald",source:"mapbox.com",key:"mapbox_emerald",uri:"https://{s}.tiles.mapbox.com/v4/mapbox.emerald/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiYmVydHQiLCJhIjoiaWFvN1Z4USJ9.k04Q6CVms4Gx7KDZxvAx2A"}
-,{active:false,nom:"MapBox High Contrast",source:"mapbox.com",key:"mapbox_contrast",uri:"https://{s}.tiles.mapbox.com/v4/mapbox.high-contrast/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiYmVydHQiLCJhIjoiaWFvN1Z4USJ9.k04Q6CVms4Gx7KDZxvAx2A"}
-,{active:false,nom:"MapBox Outdoors",source:"mapbox.com",key:"mapbox_outdoors",uri:"https://{s}.tiles.mapbox.com/v4/mapbox.outdoors/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiYmVydHQiLCJhIjoiaWFvN1Z4USJ9.k04Q6CVms4Gx7KDZxvAx2A"}
-,{active:false,nom:"MapBox Wheatpaste",source:"mapbox.com",key:"mapbox_wheatpaste",uri:"https://{s}.tiles.mapbox.com/v4/mapbox.wheatpaste/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiYmVydHQiLCJhIjoiaWFvN1Z4USJ9.k04Q6CVms4Gx7KDZxvAx2A"}
+      {baseline:"#eafafa",highlight:"#de39ac",source:"carto.com",key:"dark_all",nom:"Carto Dark",uri:"http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}@2x.png",active:true}
+      ,{baseline:"#464646",highlight:"#9572eb",source:"carto.com",key:"light_all",nom:"Carto Light",uri:"http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}@2x.png",active:false}
+      ,{baseline:"#3300b4",highlight:"#156e16",source:"esri",key:"esri_natgeo",nom:"Esri National Geographic",uri:"http://services.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}",active:false}
+      ,{baseline:"#777b49",highlight:"#db5848",source:"wmflabs.org",key:"mapnik_bw",nom:"Mapnik B/W",uri:"http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png",active:false}
+      ,{baseline:"#7b3025",highlight:"#efd3da",source:"stamen.com",key:"stamen_toner",nom:"Stamen Toner",uri:"http://tile.stamen.com/toner/{z}/{x}/{y}.png",active:false}
+      ,{baseline:"#70753a",highlight:"#31bf69",source:"stamen.com",key:"stamen_toner_lite",nom:"Stamen Toner Light",uri:"http://tile.stamen.com/toner-lite/{z}/{x}/{y}.png",active:false}
+      ,{baseline:"#5a5c58",highlight:"#26a9c8",source:"stamen.com",key:"stamen_watercolor",nom:"Stamen Watercolor",uri:"http://tile.stamen.com/watercolor/{z}/{x}/{y}.jpg",active:false}
+      ,{baseline:"#f8601b",highlight:"#798fc2",source:"waze.com",key:"waze_us",nom:"Waze US",uri:"https://livemap-tiles3.waze.com/tiles/{z}/{x}/{y}.png",active:false}
+      ,{baseline:"#404040",highlight:"#2e688d",source:"yandex.net",key:"yandex",nom:"Yandex",uri:"http://vec01.maps.yandex.net/tiles?l=map&x={x}&y={y}&z={z}",active:false}
+      ,{baseline:"#562c24",highlight:"#9d86ce",active:false,nom:"AJ Ashton's Pencil",source:"mapbox.com",key:"mapbox_ashton_pencil",uri:"http://{s}.tiles.mapbox.com/v4/aj.03e9e12d/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejdueG92YjAwZHUzMnA5ZWIyMW1zcDQifQ.acDRLVcqW0LZfWQXvC3-pw"}
+,{baseline:"#de6662",highlight:"#28660b",active:false,nom:"Pinterest by Stamen",source:"stamen.com",key:"stamen_pinterest",uri:"http://{s}.tiles.mapbox.com/v3/pinterest.map-ho21rkos/{z}/{x}/{y}.png"}
+,{baseline:"#02050b",highlight:"#584f18",active:false,nom:"Woodcut by Eleanor Lutz",source:"mapbox.com",key:"mapbox_lutz_woodcut",uri:"http://{s}.tiles.mapbox.com/v4/mapbox.b0v97egc/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejdueG92YjAwZHUzMnA5ZWIyMW1zcDQifQ.acDRLVcqW0LZfWQXvC3-pw"}
+,{baseline:"#7094b7",highlight:"#9effcf",active:false,nom:"Space Station by Eleneor Lutz",source:"mapbox.com",key:"mapbox_lutz_spacestation",uri:"http://{s}.tiles.mapbox.com/v3/eleanor.ipncow29/{z}/{x}/{y}.png"}
+,{baseline:"#b57027",highlight:"#ec9992",active:false,nom:"MapBox Financial Times",source:"mapbox.com",key:"mapbox_financialtimes",uri:"http://{s}.tiles.mapbox.com/v3/financialtimes.map-w7l4lfi8/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZHVuY2FuZ3JhaGFtIiwiYSI6IlJJcWdFczQifQ.9HUpTV1es8IjaGAf_s64VQ"}
+,{baseline:"#d15b13",highlight:"#e3e7dc",active:false,nom:"MapBox InfoAmazonia (Color)",source:"mapbox.com",key:"mapbox_infoamazonia_color",uri:"http://{s}.tiles.mapbox.com/v3/infoamazonia.map-ihpa46c3/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZHVuY2FuZ3JhaGFtIiwiYSI6IlJJcWdFczQifQ.9HUpTV1es8IjaGAf_s64VQ"}
+,{baseline:"#802816",highlight:"#75b0aa",active:false,nom:"Mapnik Mono",source:"wmflabs.org",key:"mapnik_mono",uri:"http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png"}
+,{baseline:"#89baa9",highlight:"#c8cdcc4",active:false,nom:"Blueprint by Amy Lee Walton",source:"mapbox.com",key:"mapbox_mslee_blueprint",uri:"http://api.mapbox.com/styles/v1/mslee/ciellcr9y001g5pknxuqwjhqm/tiles/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoibXNsZWUiLCJhIjoiclpiTWV5SSJ9.P_h8r37vD8jpIH1A6i1VRg"}
+,{baseline:"#707171",highlight:"#bee1e6",active:false,nom:"Winter by Amy Lee Walton",source:"mapbox.com",key:"mapbox_mslee_winter",uri:"http://api.mapbox.com/styles/v1/mslee/cij1zoclj002y8rkkdjl69psd/tiles/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoibXNsZWUiLCJhIjoiclpiTWV5SSJ9.P_h8r37vD8jpIH1A6i1VRg"}
+,{baseline:"#5a6059",highlight:"#73327c",active:false,nom:"Standard Oil by Saman Bemel Benrud",source:"mapbox.com",key:"mapbox_saman_standardoil",uri:"http://api.mapbox.com/styles/v1/saman/cif61g3y602sju1m5b02255c9/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic2FtYW4iLCJhIjoiS1ptdnd0VSJ9.19qza-F_vXkgpnh80oZJww"}
+,{baseline:"#e9a071",highlight:"#9db194",active:false,nom:"Camouflage by Eden Halperin",source:"mapbox.com",key:"mapbox_edenhalperin_camouflage",uri:"http://api.mapbox.com/styles/v1/edenhalperin/cifq0r0e5000q85m0d293k6mq/tiles/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiZWRlbmhhbHBlcmluIiwiYSI6IlFRZG0zMWMifQ.QUNKx4tIMjZfwmrE8SE6Bg"}
+,{baseline:"#8e300b",highlight:"#91aac5",active:false,nom:"MapBox Comic!",source:"mapbox.com",key:"mapbox_comic",uri:"https://{s}.tiles.mapbox.com/v4/mapbox.comic/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiYmVydHQiLCJhIjoiaWFvN1Z4USJ9.k04Q6CVms4Gx7KDZxvAx2A"}
+,{baseline:"#6c4f18",highlight:"#585c46",active:false,nom:"MapBox Emerald",source:"mapbox.com",key:"mapbox_emerald",uri:"https://{s}.tiles.mapbox.com/v4/mapbox.emerald/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiYmVydHQiLCJhIjoiaWFvN1Z4USJ9.k04Q6CVms4Gx7KDZxvAx2A"}
+,{baseline:"#c405cc",highlight:"#cc78b2",active:false,nom:"MapBox High Contrast",source:"mapbox.com",key:"mapbox_contrast",uri:"https://{s}.tiles.mapbox.com/v4/mapbox.high-contrast/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiYmVydHQiLCJhIjoiaWFvN1Z4USJ9.k04Q6CVms4Gx7KDZxvAx2A"}
+,{baseline:"#4a3c33",highlight:"#fcf5f3",active:false,nom:"MapBox Outdoors",source:"mapbox.com",key:"mapbox_outdoors",uri:"https://{s}.tiles.mapbox.com/v4/mapbox.outdoors/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiYmVydHQiLCJhIjoiaWFvN1Z4USJ9.k04Q6CVms4Gx7KDZxvAx2A"}
+,{baseline:"#ffffff",highlight:"#1ec477",active:false,nom:"MapBox Wheatpaste",source:"mapbox.com",key:"mapbox_wheatpaste",uri:"https://{s}.tiles.mapbox.com/v4/mapbox.wheatpaste/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiYmVydHQiLCJhIjoiaWFvN1Z4USJ9.k04Q6CVms4Gx7KDZxvAx2A"}
       ],
-      color_default:'white',
-      color_active:null,
       lot: null,
       bio: null,
       slug: null,
@@ -94,36 +87,39 @@ export default {
 }
   },
   computed: {
+//     palette:function(){
+// let k = this.$_.findWhere(this.basemaps, {active:true})
+
+// return k.colors;
+//     }
+// ,
 basemap:function(){
   // return this.basemaps[0];
   return this.$_.findWhere(this.basemaps, {active:true});
 }
   },
   watch: {
-  tileLayer: function() {this.routize();}
-    //lot: function() {this.routize();this.lrender();}
+  tileLayer: function() {this.routize();this.lrender()}
+    ,lot: function() {this.routize();this.lrender();}
   //,slug: function() {this.routize();}
-  //,supply: function() {this.lrender();}
+  ,supply: function() {this.lrender();}
+  // ,basemaps: function() {this.lrender();}
     },
   created() {
-  console.info(this.$route.params);
     // this.basemap=(typeof this.$route.params.basemap !== 'undefined')?this.switchMap(this.$route.params.basemap):this.$_.findWhere(this.basemaps, {active:true});
   if(typeof this.$route.params.map !== 'undefined' && this.$route.params.map!==null){this.switchMap(this.$route.params.map)}
   },
   mounted() {
-    this.color_active=(typeof this.$route.params.hue !== 'undefined')?this.$route.params.hue:'yellow';
-this.lot=(typeof this.$route.params.lot !== 'undefined')?this.$route.params.lot:null;
-    this.slug=(typeof this.$route.params.slug !== 'undefined')?this.$route.params.slug:null;
+    // this.color_active=(typeof this.$route.params.hue !== 'undefined')?this.$route.params.hue:'yellow'
+this.lot=(typeof this.$route.params.lot !== 'undefined')?this.$route.params.lot:null
+    // this.slug=(typeof this.$route.params.slug !== 'undefined')?this.$route.params.slug:null
 
-    this.initMap();
-
-
-    this.initLayers();
-    this.substyle();
+    this.initMap()
+    this.initLayers()
+    this.substyle()
 },
   methods: {
     switchMap(n,l){
-    console.log("switchmap to ",n);console.log("l is ",l);
       this.$_.each(this.basemaps,(b)=>{
         if(b.key==n){b.active=true}else{b.active=false}
       })
@@ -133,17 +129,18 @@ this.lot=(typeof this.$route.params.lot !== 'undefined')?this.$route.params.lot:
     },
     reDraw(){
 
-if(typeof this.map !== null && typeof this.map !=='undefined'){
+// if(typeof this.map !== null && typeof this.map !=='undefined'){
 this.map.removeLayer(this.tileLayer);
 
 this.tileLayer = L.tileLayer(
   this.$_.findWhere(this.basemaps, {active:true}).uri,
   {
     maxZoom: 18
+    ,isBaseMap:true
   }
 ).addTo(this.map);
 
-}
+// }
 
   }
     ,defaultStyle(){
@@ -156,12 +153,32 @@ return {
       ,opacity:.5
             }
     },
-    activeStyle(){
+    getHue(w){
+let k = this.$_.findWhere(this.basemaps, {active:true})
+let c = null
+switch(w) {
+    case 'active':
+c = k.highlight
+        break;
+    default:
+c = k.baseline
+}
+
+let style = {
+      fill:true
+      ,color:c
+      ,fillColor:c
+      ,fillOpacity:.7
+            }
+            return style
+    }
+    ,activeStyle(){
 var that=this;
+let p = this.palette
 return  {
       fill:true
-      ,color:that.color_active
-      ,fillColor:that.color_active
+      ,color:p.active
+      ,fillColor:p.active
       ,fillOpacity:.7
             }
     },
@@ -172,15 +189,13 @@ this.lots.eachLayer(function (layergrouplayer) {
 layergrouplayer.eachLayer(function(f){
 
   if(f.feature.properties.objectid==that.lot){
-      console.log("f.feature.properties.objectid v that.log")
-  console.log(f.feature.properties.objectid,that.lot)
     f.setStyle(
-     that.activeStyle()
+     that.getHue('active')
       )
     that.bio=f.feature.properties.bio
     that.map.fitBounds(f.getBounds())
   } else {
-    f.setStyle(that.defaultStyle())
+    f.setStyle(that.getHue())
   }
   f.bindPopup(f.feature.properties.siteaddres+", "+f.feature.properties.sitecity);
   f.on('click',function(e){
@@ -196,7 +211,7 @@ layergrouplayer.eachLayer(function(f){
     },
     lrender(){
 this.lots.clearLayers();
-this.lots.addLayer(L.geoJson(this.supply, {style:this.defaultStyle()}));
+this.lots.addLayer(L.geoJson(this.supply, {style:this.getHue()}));
 
 this.substyle();
 }
@@ -234,6 +249,7 @@ this.temp.msg="fetching lots..."
 axios.get('https://cecmcgee.carto.com/api/v2/sql?format=GeoJSON&q=SELECT * FROM atl_tax_parcel_parking')
     .then(response => {
       // JSON responses are automatically parsed.
+      this.temp.msg=""
       this.supply = response.data
     })
     .catch(e => {
@@ -394,14 +410,14 @@ this.clients.related.push({})
   font-weight:800;
 }
 #map{
-  height:85vh;
+  height:90vh;
   width:100%;
   position:absolute;
   margin:0;padding:0;
-  z-index:-99;
+  z-index:-1;
 }
 #map-menu{
-  height:10vh;
+  height:40px;
   /*width:30%;*/
   position:absolute;
   /*margin:0;padding:0 2%;*/
@@ -409,7 +425,7 @@ this.clients.related.push({})
   padding:.5% .5%;
   bottom:0;
   border-top-right-radius:5%;
-  background-color:rgba(255,255,255,.5);
+  background-color:rgba(255,255,255,.8);
   z-index:-98;
 }
 
@@ -428,16 +444,16 @@ border-radius:50%;
     width:27px;
   }
   #map-menu > li > .map-menu-item:hover{
-    border-color:rgb(255,255,0);
-      box-shadow: 0px 0px 2px 1px rgba(255,255,0, .2)
+    border-color:rgb(0,0,0);
+      box-shadow: 0px 0px 2px 1px rgba(0,0,0, .2)
   }
     #map-menu > li > .map-menu-item.active{
-      border-color:rgb(255,255,0);
-      box-shadow: 0px 0px 2px 1px rgba(255,255,0, .8)
+      border-color:rgb(0,0,0);
+      box-shadow: 0px 0px 2px 1px rgba(0,0,0, .8)
     }
 #notmap{
   background-color:white;
-  height:15vh;
+  height:10vh;
   z-index:0;
   position:relative;
   width:100%;
@@ -446,6 +462,11 @@ border-radius:50%;
 
 #attribution >a{
   color:inherit;text-decoration:none;
+}
+#console{
+  font-family: 'Slabo 27px', serif;
+  letter-spacing:3px;
+  font-weight:800;
 }
 
 </style>
